9b7fc9d28e4954526c293c92537974b756899e14
dearshrewdwit <dearshrewdwit@gmail.com>
2022-01-27 13:48:23 +0000

    Step 4.1: Refactor shop#updateQuality

--- src/Shop.js
+++ src/Shop.js
@@ -1,9 +1,9 @@
 const UPDATERS = {
-  'Aged Brie': require('./agedBrieUpdater'),
-  'Backstage passes to a TAFKAL80ETC concert': require('./backstagePassUpdater'),
-  'Sulfuras, Hand of Ragnaros': require('./sulfurasUpdater'),
-  'Conjured Mana Cake': require('./conjuredUpdater'),
-  'Standard': require('./standardUpdater')
+  'Aged Brie':                                  require('./agedBrieUpdater'),
+  'Backstage passes to a TAFKAL80ETC concert':  require('./backstagePassUpdater'),
+  'Sulfuras, Hand of Ragnaros':                 require('./sulfurasUpdater'),
+  'Conjured Mana Cake':                         require('./conjuredUpdater'),
+  'Standard':                                   require('./standardUpdater')
 }
 
 class Shop {
@@ -13,13 +13,11 @@ class Shop {
 
   updateQuality() {
     for (var i = 0; i < this.items.length; i++) {
-      if (this.items[i].name === 'Aged Brie') { UPDATERS['Aged Brie'](this.items[i]) }
-      if (this.items[i].name === 'Backstage passes to a TAFKAL80ETC concert') { UPDATERS['Backstage passes to a TAFKAL80ETC concert'](this.items[i]) }
-      if (this.items[i].name === 'Sulfuras, Hand of Ragnaros') { UPDATERS['Sulfuras, Hand of Ragnaros'](this.items[i]) }
-      if (this.items[i].name === 'Conjured Mana Cake') { UPDATERS['Conjured Mana Cake'](this.items[i]) }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros' && this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert' && this.items[i].name != 'Conjured Mana Cake') {
-        UPDATERS['Standard'](this.items[i])
+      let update = UPDATERS[this.items[i].name]
+      if (update === undefined) {
+        update = UPDATERS['Standard']
       }
+      update(this.items[i])
     }
 
     return this.items;
