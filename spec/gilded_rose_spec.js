70b0e6f6a545be44c4adc1445ddb8f1c1f7aa3af
phillipdunn <phillipdunn@hotmail.com>
2022-06-21 16:45:24 +0100

    add first test for quality degrades

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -1,10 +1,20 @@
-var {Shop, Item} = require('../src/gilded_rose.js');
-describe("Gilded Rose", function() {
+const { Shop, Item } = require('../src/gilded_rose.js')
 
-  it("should foo", function() {
-    const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toEqual("fixme");
-  });
+describe('Gilded Rose', function () {
+  let shop
 
-});
+  beforeEach(() => {
+    shop = new Shop([])
+  })
+  it('should show once the sell by date has passed, Quality degrades twice as fast', function () {
+    // setup
+    const item = new Item('foo', 0, 10)
+    shop.addItem(item)
+    // excute
+    const items = shop.updateQuality()
+    // verify
+    expect(items[0].name).toEqual('foo')
+    expect(items[0].sellIn).toEqual(-1)
+    expect(items[0].quality).toEqual(8)
+  })
+})
