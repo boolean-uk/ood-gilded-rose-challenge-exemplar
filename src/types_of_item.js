594e7028d222cd3edfa126a4451f69c37bfd2c6c
phillipdunn <phillipdunn@hotmail.com>
2022-06-23 15:03:02 +0100

    refactor: create item types

---
+++ src/types_of_item.js
@@ -0,0 +1,9 @@
+const TypesOfItem = {
+  agedBrie: 'Aged Brie',
+  backstagePass: 'Backstage pass',
+  conjured: 'Conjured',
+  standard: 'standard',
+  sulfuras: 'sulfuras'
+}
+
+module.exports = TypesOfItem
